e7f4b6a97f439c585da8c5366b3c9e0e516dbd22
= <=>
2021-12-24 10:22:00 +0700

    sql

--- admin/konfig.php
+++ admin/konfig.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$db = "penjualan_php";
+	$host = "10.0.0.6";
+	$user = "Admin";
+	$pass = "151617_Wahyu";
+	$db = "penjualan-buku";
 
 	$koneksi = mysqli_connect($host, $user, $pass, $db);
 
